b8eab0b3fb853f87a86bac8e002d5bddb400461f
Vikas <majokavikas@gmail.com>
2024-02-05 09:54:51 +0530

    Installing Middleware

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ const express = require('express')
 const app = express()
 const PORT=5000
 
+const customMiddleware = (req ,res ,next)=>{
+    console.log("middleware executed!!")
+    next()
+}
+
+app.use(customMiddleware)
 
 
 app.get('/' ,(req ,res)=>{
+    console.log("home")
     res.send("Hello World")
 })
 app.listen(PORT,()=>{
