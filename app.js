d2b52791c9ecbb7901e55d72ed42e00f069a96b4
Vikas <majokavikas@gmail.com>
2024-02-12 21:52:27 +0530

    Middleware-update

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const PORT=5000
 
+reuire('./models/user')
+
 const customMiddleware = (req ,res ,next)=>{
     console.log("middleware executed!!")
     next()
